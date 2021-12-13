7bb7111a898b9d47c5536c81fc950930fef565d9
hrvojekolar <hrvojekolar1998@gmail.com>
2021-12-13 14:02:48 +0100

    add lv_part_cursor

--- src/TextEntry.cpp
+++ src/TextEntry.cpp
@@ -1,20 +1,17 @@
 #include "TextEntry.h"
 #include <ByteBoi.h>
 
-static void textarea_event_handler(lv_event_t * e)
-{
-	lv_obj_t * ta = lv_event_get_target(e);
-	LV_LOG_USER("Enter was pressed. The current text is: %s", lv_textarea_get_text(ta));
-}
-
 TextEntry::TextEntry(lv_obj_t* parent, std::string text) : LVObject(parent), text(text){
 
 	obj = lv_textarea_create(lv_scr_act());
 	lv_textarea_set_placeholder_text(obj, "Hello");
 	lv_textarea_set_one_line(obj, true);
-	lv_obj_add_event_cb(obj, textarea_event_handler, LV_EVENT_READY, obj);
 	//lv_obj_set_style_border_width(obj, 2, LV_STATE_FOCUSED);
 	lv_obj_set_style_border_width(obj, 0, LV_STATE_DEFAULT);
+
+	lv_style_init(&styleFocus);
+	lv_obj_add_style(obj, &styleFocus, LV_PART_CURSOR);
+	//lv_textarea_set_cursor_pos(obj,LV_PART_CURSOR);
 
 	lv_obj_add_state(obj, LV_STATE_FOCUSED);
 	lv_obj_set_height(obj, 20);
