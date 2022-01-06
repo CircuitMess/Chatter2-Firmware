fac05eace65ae7d334fbd1cc0944c8dc76c03b2d
Emil Gajšak <emil.gajsak@gmail.com>
2022-01-06 19:40:29 +0100

    Added code testing in ino file

--- Chatter-Firmware.ino
+++ Chatter-Firmware.ino
@@ -8,7 +8,7 @@
 #include "src/User.h"
 #include <Loop/LoopManager.h>
 #include <SPIFFS.h>
-
+#include "src/EditableAvatar.h"
 
 lv_disp_draw_buf_t drawBuffer;
 Display* display;
@@ -42,15 +42,9 @@ public:
 		lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_LIGHT_BLUE), 0);
 		lv_obj_set_style_bg_opa(obj, LV_OPA_100, 0);
 
-		lv_group_add_obj(inputGroup, (new EditableAvatar(obj))->getLvObj());
+		lv_group_add_obj(inputGroup, (new EditableAvatar(obj, 0, true))->getLvObj());
 
-
-		lv_obj_t* img = lv_img_create(obj);
-		lv_img_set_src(img, "S:/test.bin");
-		lv_obj_set_style_border_width(img, 2, LV_STATE_DEFAULT);
-		lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
-		lv_obj_set_size(img, 160, 128);
-		Serial.println("----------------");
+		lv_group_add_obj(inputGroup, (new EditableAvatar(obj, 0, true))->getLvObj());
 
 /*		for(int i = 0; i < 5; i++){
 			User* user = new User(obj, esp_random() % 360, "Foo " + String(i + 1));
@@ -69,7 +63,6 @@ void setup(){
 	Serial.begin(115200);
 	Chatter.begin();
 	display = Chatter.getDisplay();
-
 	lv_init();
 	lv_disp_draw_buf_init(&drawBuffer, display->getBaseSprite()->getBuffer(), NULL, 160 * 128);
 //	lv_log_register_print_cb(my_print); /* register print function for debugging */
@@ -82,6 +75,7 @@ void setup(){
 	displayDriver.flush_cb = lvglFlush;
 	displayDriver.draw_buf = &drawBuffer;
 	lv_disp_drv_register(&displayDriver);
+
 
 	new FSLVGL(SPIFFS, 'S');
 
