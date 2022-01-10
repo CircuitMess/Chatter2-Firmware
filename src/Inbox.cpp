3d1aa19549e00af0b42c3ab1372e7ca0c128f6a1
Emil Gajšak <emil.gajsak@gmail.com>
2022-01-10 18:04:24 +0100

    Removed duplicate font, added Inbox redraw on focus shift

--- src/Inbox.cpp
+++ src/Inbox.cpp
@@ -58,6 +58,7 @@ Inbox::Inbox() : LVScreen(){
 	lv_group_set_focus_cb(inputGroup, [](lv_group_t* group){
 		lv_obj_t* focused = lv_group_get_focused(group);
 		lv_obj_scroll_to_view(focused, LV_ANIM_ON);
+		lv_obj_invalidate(lv_obj_get_parent(focused));
 	});
 
 }
