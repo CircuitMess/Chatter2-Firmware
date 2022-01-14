5800fba8d3d7931382aaca013e77c62d84471d72
hrvojekolar <hrvojekolar1998@gmail.com>
2022-01-14 17:41:27 +0100

    Separate color layout and slider layout in ColorBoxLVGL

--- src/ColorBoxLVGL.cpp
+++ src/ColorBoxLVGL.cpp
@@ -2,48 +2,55 @@
 
 
 ColorBox::ColorBox(lv_obj_t* parent, uint16_t hue) : LVObject(parent), color(color){
+	lv_obj_set_size(obj,50,20);
+	lv_obj_set_style_bg_opa(obj,0,0);
+	static const lv_style_prop_t props[] = {LV_STYLE_BG_COLOR};
+
+	static lv_style_t style_colorPicker;
+	static lv_style_t style_knob;
+
 	lv_style_selector_t sel = LV_PART_MAIN | LV_STATE_DEFAULT;
 	lv_style_selector_t selFocus = LV_PART_MAIN | LV_STATE_FOCUSED;
-	lv_style_selector_t selPress = LV_PART_MAIN | LV_STATE_PRESSED;
-
-	lv_obj_set_size(obj,50,20);
-
-	static const lv_style_prop_t props[] = {LV_STYLE_BG_COLOR};
-	static lv_style_transition_dsc_t transition_dsc;
-	lv_style_transition_dsc_init(&transition_dsc, props, lv_anim_path_linear, 300, 0, NULL);
-
-	static lv_style_t style_main;
-	static lv_style_t style_knob;
-	static lv_style_t style_pressed_color;
-	
-	lv_style_init(&style_main);
-	lv_style_set_bg_opa(&style_main, LV_OPA_COVER);
-	lv_style_set_bg_img_src(&style_main, "S:/colorPicker.bin");
 
 	lv_style_init(&style_knob);
-	lv_style_set_bg_opa(&style_knob, LV_OPA_COVER);
+	lv_style_set_bg_opa(&style_knob, 0);
 	lv_style_set_bg_img_src(&style_knob, "S:/arrowKnob.bin");
+	
+	lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_COLUMN);
+	lv_obj_set_flex_align(obj, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START);
 
-	/*Create a slider and add the style*/
-	lv_obj_t * slider = lv_slider_create(obj);
+	slider = lv_slider_create(obj);
+	lv_obj_add_flag(slider, LV_OBJ_FLAG_HIDDEN);
 	lv_obj_remove_style_all(slider);        /*Remove the styles coming from the theme*/
-	lv_obj_set_width(slider,50);
-	lv_obj_set_height(slider,10);
-	lv_obj_add_style(slider, &style_main, LV_PART_MAIN);
+	lv_obj_set_width(slider,46);
+	lv_obj_set_height(slider,8);
 	lv_obj_add_style(slider, &style_knob, LV_PART_KNOB);
-	lv_slider_set_value(slider,20,LV_ANIM_OFF);
-	//lv_obj_add_style(slider, &style_pressed_color, LV_PART_KNOB | LV_STATE_PRESSED);
+	lv_style_set_width(&style_knob,1);
+	lv_style_set_height(&style_knob,1);
 	lv_obj_center(slider);
+	lv_slider_set_value(slider,20,LV_ANIM_OFF);
 
-	lv_obj_set_style_translate_y(slider, 5,LV_PART_KNOB);
+	lv_style_init(&style_colorPicker);
+	lv_style_set_bg_opa(&style_colorPicker, selFocus);
+	lv_style_set_bg_img_src(&style_colorPicker, "S:/colorPicker.bin");
+
+	colorStrip = lv_obj_create(obj);
+	lv_obj_set_size(colorStrip, 46, 8);
+	lv_obj_set_style_bg_color(colorStrip, lv_color_hsv_to_rgb(0, 100, 100), sel);
+
+	lv_obj_set_style_border_width(colorStrip,1,selFocus);
+	lv_obj_set_style_border_color(colorStrip,lv_color_white(),selFocus);
+	lv_obj_add_style(colorStrip, &style_colorPicker, selFocus);
+	lv_style_set_width(&style_colorPicker,46);
+	lv_style_set_height(&style_colorPicker,8);
+
+	//lv_img_set_src(colorStrip, "S:/colorPicker.bin");
+	lv_obj_align(colorStrip, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_set_style_opa(colorStrip,LV_OPA_COVER,selFocus);
+	lv_obj_set_size(colorStrip, 46, 8);
 
 //------------------------------------------
 
-	/*lv_obj_t* img = lv_img_create(obj);
-	lv_img_set_src(img, "S:/colorPicker.bin");
-	lv_obj_set_style_border_width(img, 1, LV_STATE_DEFAULT);
-	lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
-	lv_obj_set_size(img, 46, 8);*/
 
 	/*lv_style_init(&styleDef);
 	lv_style_set_bg_color(&styleDef, color);
