aa62a935a4813a5b8ba8b0f1f851dda6cd2187ed
Filip Budiša <filip@circuitmess.com>
2022-01-18 17:04:49 +0100

    Clean up TextEntry, add setters and getters

--- src/TextEntry.h
+++ src/TextEntry.h
@@ -6,17 +6,40 @@
 #include "LVObject.h"
 #include <string>
 #include <Input/InputListener.h>
+#include <Loop/LoopListener.h>
 
-class TextEntry : public LVObject, private InputListener{
+class TextEntry : public LVObject, private InputListener, public LoopListener {
 public:
-	TextEntry(lv_obj_t* parent, std::string text = "");
+	TextEntry(lv_obj_t* parent, const std::string& text = "");
+	void setTextColor(lv_color_t color);
+	void setPlaceholder(const std::string& text);
+	void setText(const std::string& text);
+	const std::string& getText() const;
+
+	void keyPress(uint8_t i);
+	void clear();
+
+	void start();
+	void stop();
+	bool isActive() const;
+
+	void loop(uint micros) override;
 
 private:
+	void buttonPressed(uint i) override;
+
+	static const char* characters[];
+	static const std::map<uint8_t, uint8_t> keyMap;
+
 	std::string text;
 
-protected:
-	lv_style_t styleFocus;
-	lv_style_t styleDef;
+	lv_style_t entryFocus;
+	lv_obj_t* entry;
+	bool active = false;
+
+	int8_t currentKey = -1; // currently active key
+	uint8_t index = 0; // character under the key
+	uint32_t keyTime = 0; // when the key was last pressed
 };
 
 #endif //CHATTER_FIRMWARE_TEXTENTRY_H
