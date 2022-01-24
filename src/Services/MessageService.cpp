d9ac6080ec47211c115786586b477ca07f4a47ea
Filip Budiša <filip@circuitmess.com>
2022-01-24 18:16:07 +0100

    Add MessageService receiving

--- src/Services/MessageService.cpp
+++ src/Services/MessageService.cpp
@@ -1,6 +1,13 @@
+#include <Loop/LoopManager.h>
 #include "MessageService.h"
 #include "LoRaService.h"
 #include "../Storage/Storage.h"
+
+MessageService Messages;
+
+void MessageService::begin(){
+	LoopManager::addListener(this);
+}
 
 bool MessageService::sendText(Convo& convo, const std::string& text){
 	Message message;
@@ -23,52 +30,99 @@ bool MessageService::sendMessage(Convo& convo, Message& message){
 
 	message.outgoing = true;
 
-	LoRa.send(convo.uid, LoRaPacket::Type::MSG, &message, sizeof(message));
+	if(!sendPacket(convo.uid, message)) return false;
 
 	if(!Storage.Messages.add(message)) return false;
+
+	convo.messages.push_back(message.uid);
 	if(!Storage.Convos.update(convo)) return false;
 
+	return true;
+}
+
+bool MessageService::sendPacket(UID_t receiver, const Message& message){
+	MessagePacket* packet;
+
+	if(message.getType() == Message::TEXT){
+		packet = new TextMessage(message.getText());
+	}else if(message.getType() == Message::PIC){
+		packet = new PicMessage(message.getPic());
+	}else{
+		return false;
+	}
+
+	packet->uid = message.uid;
+	LoRa.send(receiver, LoRaPacket::MSG, packet);
+
+	delete packet;
 	return true;
 }
 
 void MessageService::loop(uint micros){
 	ReceivedPacket<MessagePacket> packet = LoRa.getMessage();
 
-	if(packet.content && Storage.Friends.exists(packet.sender)){
-		if(packet.content->type == MessagePacket::ACK){
-			// TODO
-		}else{
-			Message message;
-			message.uid = packet.content->uid;
-			message.outgoing = false;
-			message.convo = packet.sender;
+	if(!packet.content || !Storage.Friends.exists(packet.sender)) return;
 
-			if(packet.content->type == MessagePacket::TEXT){
-				TextMessage* text = dynamic_cast<TextMessage*>(packet.content);
-				message.setText(text->text);
-				delete text;
-			}else if(packet.content->type == MessagePacket::PIC){
-				PicMessage* pic = reinterpret_cast<PicMessage*>(packet.content);
-				message.setPic(pic->index);
-				delete pic;
-			}
-
-			Convo convo = Storage.Convos.get(packet.sender);
-			if(convo.uid == 0){
-				convo.uid = packet.sender;
-				Storage.Convos.add(convo);
-			}
-
-			Storage.Messages.add(message);
-			Storage.Convos.update(convo);
-
-			// TODO: call new message listeners
-		}
-
-		MessagePacket ack;
-		ack.type = MessagePacket::ACK;
-		ack.uid = packet.content->uid;
-
-		LoRa.send(packet.sender, LoRaPacket::Type::MSG, &ack, sizeof(ack));
+	if(packet.content->type == MessagePacket::ACK){
+		receiveAck(packet);
+	}else{
+		receiveMessage(packet);
 	}
+}
+
+void MessageService::receiveMessage(ReceivedPacket<MessagePacket>& packet){
+	Message message;
+	message.uid = packet.content->uid;
+	message.outgoing = false;
+	message.convo = packet.sender;
+
+	if(packet.content->type == MessagePacket::TEXT){
+		TextMessage* text = reinterpret_cast<TextMessage*>(packet.content);
+		message.setText(text->text);
+		delete text;
+	}else if(packet.content->type == MessagePacket::PIC){
+		PicMessage* pic = reinterpret_cast<PicMessage*>(packet.content);
+		message.setPic(pic->index);
+		delete pic;
+	}else{
+		return;
+	}
+
+	Convo convo = Storage.Convos.get(packet.sender);
+	if(convo.uid == 0){
+		convo.uid = packet.sender;
+		Storage.Convos.add(convo);
+	}
+
+	convo.messages.push_back(message.uid);
+
+	if(!Storage.Messages.add(message)){
+		printf("Error adding message\n");
+	}
+	if(!Storage.Convos.update(convo)){
+		printf("Error updating convo\n");
+	}
+
+	// TODO: call new message listeners
+
+	MessagePacket ack;
+	ack.type = MessagePacket::ACK;
+	ack.uid = message.uid;
+
+	LoRa.send(packet.sender, LoRaPacket::Type::MSG, &ack);
+}
+
+void MessageService::receiveAck(ReceivedPacket<MessagePacket>& packet){
+	UID_t uid = packet.content->uid;
+	delete packet.content;
+
+	Message msg = Storage.Messages.get(uid);
+	if(msg.uid == 0) return;
+
+	msg.received = true;
+	if(!Storage.Messages.update(msg)){
+		printf("Message ACK update failed\n");
+	}
+
+	// TODO: call listeners
 }
--- src/Services/MessageService.h
+++ src/Services/MessageService.h
@@ -5,6 +5,7 @@
 #include "../Types.hpp"
 #include "../Model/Message.h"
 #include "../Model/Convo.hpp"
+#include "LoRaPacket.h"
 #include <string>
 #include <Loop/LoopListener.h>
 
@@ -13,12 +14,18 @@ public:
 	bool sendText(Convo& convo, const std::string& text);
 	bool sendPic(Convo& convo, uint16_t index);
 
+	void begin();
 	void loop(uint micros) override;
 
 private:
 	bool sendMessage(Convo& convo, Message& message);
+	bool sendPacket(UID_t receiver, const Message& message);
+
+	void receiveMessage(ReceivedPacket<MessagePacket>& packet);
+	void receiveAck(ReceivedPacket<MessagePacket>& packet);
 
 };
 
+extern MessageService Messages;
 
 #endif //CHATTER_FIRMWARE_MESSAGESERVICE_H
