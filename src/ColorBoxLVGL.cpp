ecd16860170836b36b0155c074c5214f2e52ba80
Emil Gajšak <emil.gajsak@gmail.com>
2022-01-24 22:31:17 +0100

    Reworked ColorBox

--- src/ColorBoxLVGL.cpp
+++ src/ColorBoxLVGL.cpp
@@ -1,100 +1,119 @@
+#include <Pins.hpp>
 #include "ColorBoxLVGL.h"
+#include <Input/Input.h>
 
-
-ColorBox::ColorBox(lv_obj_t* parent, uint16_t hue) : LVObject(parent), color(color){
-	lv_obj_set_size(obj, 50, 20);
-	lv_obj_set_style_bg_opa(obj, 0, 0);
-	static const lv_style_prop_t props[] = {LV_STYLE_BG_COLOR};
-
-	static lv_style_t style_colorPicker;
-	static lv_style_t style_knob;
+ColorBox::ColorBox(lv_obj_t* parent, uint16_t hue){
 
 	lv_style_selector_t sel = LV_PART_MAIN | LV_STATE_DEFAULT;
 	lv_style_selector_t selFocus = LV_PART_MAIN | LV_STATE_FOCUSED;
 
-	lv_style_init(&style_knob);
-	lv_style_set_bg_opa(&style_knob, 0);
-	lv_style_set_bg_img_src(&style_knob, "S:/arrowKnob.bin");
+	setButtonHoldAndRepeatTime(BTN_LEFT, 50);
+	setButtonHoldAndRepeatTime(BTN_RIGHT, 50);
+	setButtonHoldTime(BTN_LEFT, 400);
+	setButtonHoldTime(BTN_RIGHT, 400);
 
-	lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_COLUMN);
-	lv_obj_set_flex_align(obj, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START);
-
-	slider = lv_slider_create(obj);
-	lv_obj_add_flag(slider, LV_OBJ_FLAG_HIDDEN);
+	slider = lv_slider_create(parent);
+	lv_slider_set_range(slider, 0, 45);    // 360 = 45 * 8
+	lv_slider_set_value(slider, hue / 8, LV_ANIM_OFF);
 	lv_obj_remove_style_all(slider);        /*Remove the styles coming from the theme*/
-	lv_obj_set_width(slider, 46);
-	lv_obj_set_height(slider, 8);
-	lv_obj_add_style(slider, &style_knob, LV_PART_KNOB);
-	lv_obj_set_style_pad_hor(slider,1,0);
-	lv_style_set_width(&style_knob, 1);
-	lv_style_set_height(&style_knob, 1);
-	lv_obj_center(slider);
-	//lv_slider_set_value(slider, 20, LV_ANIM_OFF);
+	lv_obj_set_size(slider, 45, 20);
 
+	//knob
+	lv_style_init(&style_knob);
+	lv_style_set_bg_opa(&style_knob, LV_OPA_0);
+	lv_style_set_bg_img_src(&style_knob, LV_SYMBOL_DOWN);
+	lv_style_set_width(&style_knob, 10);
+	lv_style_set_height(&style_knob, 10);
+	lv_style_set_pad_top(&style_knob, 13);
+	lv_style_set_text_font(&style_knob, &lv_font_montserrat_8);
+	lv_style_set_text_color(&style_knob, lv_color_white());
+	lv_obj_add_style(slider, &style_knob, LV_PART_KNOB | LV_STATE_EDITED);
+
+	//edited state
 	lv_style_init(&style_colorPicker);
-	lv_style_set_bg_opa(&style_colorPicker, selFocus);
+	lv_style_set_bg_opa(&style_colorPicker, LV_OPA_0);
 	lv_style_set_bg_img_src(&style_colorPicker, "S:/colorPicker.bin");
+	lv_style_set_border_width(&style_colorPicker, 0);
+	lv_obj_add_style(slider, &style_colorPicker, LV_PART_MAIN | LV_STATE_EDITED);
 
-	colorStrip = lv_obj_create(obj);
-	lv_obj_set_size(colorStrip, 46, 8);
-	lv_obj_set_style_bg_color(colorStrip, lv_color_hsv_to_rgb(0, 100, 100), sel);
+	//default state
+	lv_style_init(&style_def);
+	lv_style_set_bg_opa(&style_def, LV_OPA_100);
+	lv_style_set_bg_color(&style_def, lv_palette_main(LV_PALETTE_RED));
+	lv_style_set_pad_all(&style_def, 1);
+	lv_style_set_border_width(&style_def, 0);
+	lv_style_set_height(&style_def, 20);
+	lv_obj_add_style(slider, &style_def, sel);
 
-	lv_obj_set_style_border_width(colorStrip, 1, selFocus);
-	lv_obj_set_style_border_color(colorStrip, lv_color_white(), selFocus);
-	lv_obj_add_style(colorStrip, &style_colorPicker, selFocus);
-	lv_style_set_width(&style_colorPicker, 46);
-	lv_style_set_height(&style_colorPicker, 8);
+	//focused state
+	lv_style_init(&style_focused);
+	lv_style_set_border_width(&style_focused, 1);
+	lv_style_set_border_color(&style_focused, lv_color_white());
+	lv_style_set_border_opa(&style_focused, LV_OPA_100);
+	lv_style_set_pad_all(&style_focused, 0);
+	lv_obj_add_style(slider, &style_focused, selFocus);
 
-	//lv_img_set_src(colorStrip, "S:/colorPicker.bin");
-	lv_obj_align(colorStrip, LV_ALIGN_CENTER, 0, 0);
-	lv_obj_set_style_opa(colorStrip, LV_OPA_COVER, selFocus);
-	lv_obj_set_size(colorStrip, 46, 8);
+	setColor(hue);
 
-	lv_group_add_obj((lv_group_t*) lv_obj_get_group(obj), slider);
-
-	lv_obj_add_event_cb(obj, [](lv_event_t* event){
-		static_cast<ColorBox*>(lv_event_get_user_data(event))->selected();
+	lv_obj_add_event_cb(slider, [](lv_event_t* event){
+		Input::getInstance()->addListener(static_cast<ColorBox*>(lv_event_get_user_data(event)));
 	}, LV_EVENT_FOCUSED, this);
 
-	lv_obj_add_event_cb(obj, [](lv_event_t* event){
-		static_cast<ColorBox*>(lv_event_get_user_data(event))->deselected();
+	lv_obj_add_event_cb(slider, [](lv_event_t* event){
+		Input::getInstance()->removeListener(static_cast<ColorBox*>(lv_event_get_user_data(event)));
 	}, LV_EVENT_DEFOCUSED, this);
 
-	lv_obj_add_event_cb(obj, [](lv_event_t* event){
-		static_cast<ColorBox*>(lv_event_get_user_data(event))->clicked();
-	}, LV_EVENT_CLICKED, this);
-
-	lv_obj_add_event_cb(obj, [](lv_event_t* event){
-		uint32_t c = lv_event_get_key(event);
-		auto sliderEvent = static_cast<ColorBox*>(event->user_data);
-		if(c == LV_KEY_ESC){
-			sliderEvent->exit();
+	lv_obj_add_event_cb(slider, [](lv_event_t* event){
+		if(lv_event_get_key(event) == LV_KEY_ESC){
+			lv_group_set_editing((lv_group_t*)lv_obj_get_group(lv_event_get_target(event)), false);
 		}
 	}, LV_EVENT_KEY, this);
+
+	lv_obj_add_event_cb(slider, [](lv_event_t* event){
+		static_cast<ColorBox*>(lv_event_get_user_data(event))->setColor(lv_slider_get_value(lv_event_get_target(event)) * 8);
+	}, LV_EVENT_VALUE_CHANGED, this);
+
+	lv_obj_add_event_cb(slider, [](lv_event_t* event){
+		delete static_cast<ColorBox*>(lv_event_get_user_data(event));
+	}, LV_EVENT_DELETE, this);
 }
 
-void ColorBox::selected(){
-	lv_obj_set_style_border_width(colorStrip,1,0);
-	lv_obj_set_style_border_color(colorStrip,lv_color_white(),0);
-	lv_group_set_editing((lv_group_t*)lv_obj_get_group(slider), false);
-
+ColorBox::~ColorBox(){
+	Input::getInstance()->removeListener(this);
 }
 
-void ColorBox::deselected(){
-	lv_obj_set_style_border_width(colorStrip,0,0);
+lv_obj_t* ColorBox::getLvObj(){
+	return slider;
 }
 
-void ColorBox::clicked(){
-	lv_obj_clear_flag(slider, LV_OBJ_FLAG_HIDDEN);
-	lv_obj_add_state(colorStrip, LV_STATE_FOCUSED);
-	lv_obj_add_state(slider, LV_STATE_EDITED);
-	lv_group_focus_obj(slider);
-	lv_group_set_editing((lv_group_t*)lv_obj_get_group(slider), true);
+void ColorBox::setColor(uint16_t hue){
+	this->hue = hue;
+	lv_style_set_bg_color(&style_def, lv_color_hsv_to_rgb(hue, 100, 100));
 }
 
-void ColorBox::exit(){
-	lv_obj_add_flag(slider, LV_OBJ_FLAG_HIDDEN);
-	lv_obj_clear_state(colorStrip, LV_STATE_FOCUSED);
-	lv_group_focus_obj(obj);
-	lv_group_set_editing((lv_group_t*)lv_obj_get_group(slider), false);
+void ColorBox::buttonHeldRepeat(uint i, uint repeatCount){
+	if(!lv_obj_has_state(slider, LV_STATE_EDITED) || !heldThresh) return;
+
+	if(i == BTN_LEFT && lv_slider_get_value(slider) > 0){
+		lv_slider_set_value(slider, lv_slider_get_value(slider) - 1, LV_ANIM_ON);
+	}else if(i == BTN_RIGHT && lv_slider_get_value(slider) < 45){
+		lv_slider_set_value(slider, lv_slider_get_value(slider) + 1, LV_ANIM_ON);
+	}
+	setColor(lv_slider_get_value(slider) * 8);
+}
+
+void ColorBox::buttonReleased(uint i){
+	if(!lv_obj_has_state(slider, LV_STATE_EDITED) && heldThresh) return;
+
+	if(i == BTN_LEFT || i == BTN_RIGHT){
+		heldThresh = false;
+	}
+}
+
+void ColorBox::buttonHeld(uint i){
+	if(!lv_obj_has_state(slider, LV_STATE_EDITED) && !heldThresh) return;
+
+	if(i == BTN_LEFT || i == BTN_RIGHT){
+		heldThresh = true;
+	}
 }
--- src/ColorBoxLVGL.h
+++ src/ColorBoxLVGL.h
@@ -4,22 +4,31 @@
 #include <Arduino.h>
 #include <lvgl.h>
 #include "LVObject.h"
+#include <Input/InputListener.h>
 
-class ColorBox : public LVObject{
+class ColorBox : public InputListener{
 public:
 	ColorBox(lv_obj_t *parent,uint16_t hue);
-	void selected();
-	void deselected();
-	void clicked();
-	void exit();
-private:
-	lv_color_t color;
+	virtual ~ColorBox();
+	lv_obj_t* getLvObj();
+	void setColor(uint16_t hue);
+
 protected:
 	lv_style_t styleFocus;
 	lv_style_t styleDef;
-
+	uint16_t hue;
 	lv_obj_t* slider;
-	lv_obj_t* colorStrip;
+
+private:
+	lv_style_t style_colorPicker;
+	lv_style_t style_knob;
+	lv_style_t style_def;
+	lv_style_t style_focused;
+
+	bool heldThresh = false;
+	void buttonHeldRepeat(uint i, uint repeatCount) override;
+	void buttonHeld(uint i) override;
+	void buttonReleased(uint i) override;
 };
 
 #endif //CHATTER_FIRMWARE_COLORBOXLVGL_H
