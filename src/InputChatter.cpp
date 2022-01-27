e62fe47eb9cebba416d0583f840d5611271e1785
Filip Budiša <filip@circuitmess.com>
2022-01-27 20:55:49 +0100

    Set keypad input device type

--- src/InputChatter.cpp
+++ src/InputChatter.cpp
@@ -3,14 +3,16 @@
 #include <Chatter.h>
 #include <vector>
 
-std::map<uint8_t, lv_key_t> InputChatter::keyMap = {{BTN_UP,   LV_KEY_LEFT},
-													{BTN_DOWN, LV_KEY_RIGHT},
-													{BTN_A,    LV_KEY_ENTER},
-													{BTN_B,    LV_KEY_ESC},
-													{BTN_C,    LV_KEY_DEL}};
+const std::map<uint8_t, lv_key_t> InputChatter::keyMap = {
+		{ BTN_LEFT,  LV_KEY_PREV },
+		{ BTN_RIGHT, LV_KEY_NEXT },
+		{ BTN_ENTER, LV_KEY_ENTER },
+		{ BTN_BACK,  LV_KEY_ESC },
+		{ BTN_R,     LV_KEY_DEL }
+};
 
 
-InputChatter::InputChatter() : InputLVGL(LV_INDEV_TYPE_ENCODER){
+InputChatter::InputChatter() : InputLVGL(LV_INDEV_TYPE_KEYPAD){
 
 }
 
