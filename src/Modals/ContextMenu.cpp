47afdac051e37b9f9234fc2bd3148e1014a70105
Filip Budiša <filip@circuitmess.com>
2022-01-27 22:39:01 +0100

    Adjust ContextMenu styles

--- src/Modals/ContextMenu.cpp
+++ src/Modals/ContextMenu.cpp
@@ -8,40 +8,36 @@ ContextMenu::ContextMenu(lv_obj_t* parent, const std::vector<std::string>& optio
 
 	lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_COLUMN);
 	lv_obj_set_flex_align(obj, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
-	lv_obj_set_style_bg_color(obj, lv_color_hsv_to_rgb(0, 0, 50), 0);
-	lv_obj_set_style_bg_opa(obj, LV_OPA_COVER, 0);
-	lv_obj_set_style_pad_gap(obj, 0, 0);
-	lv_obj_set_style_border_width(obj, 1, 0);
-	lv_obj_set_style_border_color(obj, lv_color_white(), 0);
 
 	lv_style_init(&styleDef);
-	lv_style_set_bg_color(&styleDef,lv_color_hsv_to_rgb(326, 20, 64));
 	lv_style_set_bg_opa(&styleDef, LV_OPA_COVER);
+	lv_style_set_text_font(&styleDef, &pixelbasic_7);
+	lv_style_set_text_color(&styleDef, lv_color_white());
+	lv_style_set_bg_color(&styleDef,lv_color_hsv_to_rgb(326, 20, 64));
+	lv_style_set_border_color(&styleDef, lv_color_white());
+	lv_style_set_border_width(&styleDef, 1);
+	lv_style_set_pad_hor(&styleDef, 2);
+	lv_style_set_pad_ver(&styleDef, 3);
 
 	lv_style_init(&styleFocus);
 	lv_style_set_bg_color(&styleFocus, lv_color_hsv_to_rgb(325, 91, 55));
-	lv_style_set_bg_opa(&styleFocus, LV_OPA_COVER);
+	lv_style_set_border_width(&styleFocus, 2);
+	lv_style_set_pad_hor(&styleFocus, 1);
+	lv_style_set_pad_ver(&styleFocus, 2);
 
 	lv_obj_set_width(obj, lv_pct(70));
 	lv_obj_set_height(obj, LV_SIZE_CONTENT);
 
 	menuInputGroup = lv_group_create();
 
-	for(int i = 0; i < options.size(); ++i){
-		lv_style_set_text_font(&styleFocus, &pixelbasic_7);
-		lv_style_set_text_font(&styleDef, &pixelbasic_7);
-		lv_style_set_text_color(&styleDef, lv_color_white());
-
+	for(const auto& text : options){
 		labelObj = lv_label_create(obj);
-		lv_label_set_text(labelObj, options[i].c_str());
 		labelVector.push_back(labelObj);
+		lv_label_set_text(labelObj, text.c_str());
 
 		lv_obj_set_width(labelObj, lv_pct(100));
 		lv_obj_set_height(labelObj, LV_SIZE_CONTENT);
-		lv_obj_set_style_pad_all(labelObj, 2, 0);
 
-		lv_obj_set_style_border_width(labelObj, 1, 0);
-		lv_obj_set_style_border_color(labelObj, lv_color_white(), 0);
 		lv_obj_add_style(labelObj, &styleFocus, LV_STATE_FOCUSED);
 		lv_obj_add_style(labelObj, &styleDef, LV_STATE_DEFAULT);
 
