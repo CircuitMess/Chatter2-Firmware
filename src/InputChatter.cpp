3df9a2cf0c59f257742d05e44e263f229e3fa3eb
Filip Budiša <filip@circuitmess.com>
2022-01-27 22:07:55 +0100

    Fix InputChatter KeyMap lookup

--- src/InputChatter.cpp
+++ src/InputChatter.cpp
@@ -3,7 +3,7 @@
 #include <Chatter.h>
 #include <vector>
 
-const std::map<uint8_t, lv_key_t> InputChatter::keyMap = {
+const std::map<uint8_t, lv_key_t> InputChatter::KeyMap = {
 		{ BTN_LEFT,  LV_KEY_PREV },
 		{ BTN_RIGHT, LV_KEY_NEXT },
 		{ BTN_ENTER, LV_KEY_ENTER },
@@ -19,7 +19,10 @@ InputChatter::InputChatter() : InputLVGL(LV_INDEV_TYPE_KEYPAD){
 void InputChatter::read(lv_indev_drv_t* drv, lv_indev_data_t* data){
 	if(lastKey == (uint32_t)-1) return;
 
-	data->key = keyMap[lastKey];
+	auto key = KeyMap.find(lastKey);
+	if(key == KeyMap.end()) return;
+
+	data->key = key->second;
 	data->state = pressed ? LV_INDEV_STATE_PRESSED : LV_INDEV_STATE_RELEASED;
 }
 
