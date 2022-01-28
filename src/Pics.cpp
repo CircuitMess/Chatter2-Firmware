1e2ddecfa3e38170e3453035510820c380df82b2
Filip Budiša <filip@circuitmess.com>
2022-01-28 17:08:02 +0100

    Add Pic::create

--- src/Pics.cpp
+++ src/Pics.cpp
@@ -23,3 +23,18 @@ bool Pic::gif() const{
 	str.toLowerCase();
 	return str.endsWith(".gif");
 }
+
+lv_obj_t* Pic::create(lv_obj_t* parent) const{
+	String path = String("S:/Pics/") + name;
+
+	lv_obj_t* pic;
+	if(gif()){
+		pic = lv_gif_create(parent);
+		lv_gif_set_src(pic, path.c_str());
+	}else{
+		pic = lv_img_create(parent);
+		lv_img_set_src(pic, path.c_str());
+	}
+
+	return pic;
+}
