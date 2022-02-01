5e4e1f81c0b477de09c054a1ff74be9ce423cd6f
Filip Budiša <filip@circuitmess.com>
2022-02-01 12:24:39 +0100

    Adjust ContextMenu borders

--- src/Modals/ContextMenu.cpp
+++ src/Modals/ContextMenu.cpp
@@ -8,22 +8,26 @@ ContextMenu::ContextMenu(LVScreen* parent, const std::vector<Option>& options) :
 
 	lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_COLUMN);
 	lv_obj_set_flex_align(obj, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
+	lv_obj_set_style_pad_all(obj, 1, 0);
+	lv_obj_set_style_pad_gap(obj, 1, 0);
+	lv_obj_set_style_bg_color(obj, lv_color_white(), 0);
+	lv_obj_set_style_bg_opa(obj, LV_OPA_COVER, 0);
 
 	lv_style_init(&styleDef);
 	lv_style_set_bg_opa(&styleDef, LV_OPA_COVER);
 	lv_style_set_text_font(&styleDef, &pixelbasic_7);
 	lv_style_set_text_color(&styleDef, lv_color_white());
 	lv_style_set_bg_color(&styleDef,lv_color_hsv_to_rgb(326, 20, 64));
-	lv_style_set_border_color(&styleDef, lv_color_white());
-	lv_style_set_border_width(&styleDef, 1);
-	lv_style_set_pad_hor(&styleDef, 2);
-	lv_style_set_pad_ver(&styleDef, 3);
+	lv_style_set_pad_hor(&styleDef, 3);
+	lv_style_set_pad_ver(&styleDef, 4);
 
 	lv_style_init(&styleFocus);
 	lv_style_set_bg_color(&styleFocus, lv_color_hsv_to_rgb(325, 91, 55));
-	lv_style_set_border_width(&styleFocus, 2);
-	lv_style_set_pad_hor(&styleFocus, 1);
-	lv_style_set_pad_ver(&styleFocus, 2);
+	lv_style_set_border_opa(&styleFocus, LV_OPA_COVER);
+	lv_style_set_border_width(&styleFocus, 1);
+	lv_style_set_border_color(&styleFocus, lv_color_white());
+	lv_style_set_pad_hor(&styleFocus, 2);
+	lv_style_set_pad_ver(&styleFocus, 3);
 
 	lv_obj_set_width(obj, 110);
 	lv_obj_set_height(obj, LV_SIZE_CONTENT);
