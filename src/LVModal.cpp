de07c246e6d120647f7a3a89baf5dca3c95bc209
Filip Budiša <filip@circuitmess.com>
2022-02-01 12:24:11 +0100

    Adjust modal shadow

--- src/LVModal.cpp
+++ src/LVModal.cpp
@@ -13,9 +13,10 @@ LVModal::LVModal(LVScreen* parent) : LVObject(parent->getLvObj()), parentScreen(
 	lv_obj_set_align(container, LV_ALIGN_CENTER);
 	lv_obj_add_flag(container, LV_OBJ_FLAG_HIDDEN);
 
-	lv_obj_set_style_border_color(container, lv_color_black(), 0);
-	lv_obj_set_style_border_width(container, 2, 0);
-	lv_obj_set_style_border_opa(container, LV_OPA_20, 0);
+	lv_obj_set_style_shadow_width(container, 6, 0);
+	lv_obj_set_style_shadow_color(container, lv_color_black(), 0);
+	lv_obj_set_style_shadow_spread(container, 0, 0);
+	lv_obj_set_style_shadow_opa(container, LV_OPA_COVER, 0);
 }
 
 LVModal::~LVModal(){
