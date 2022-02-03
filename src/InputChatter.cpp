786ae36df84d5159fc44c946fc3198e6588f24e7
Filip Budiša <filip@circuitmess.com>
2022-02-03 13:32:37 +0100

    Revert "Set keypad input device type" This reverts commit e62fe47e

--- src/InputChatter.cpp
+++ src/InputChatter.cpp
@@ -3,26 +3,21 @@
 #include <Chatter.h>
 #include <vector>
 
-const std::map<uint8_t, lv_key_t> InputChatter::KeyMap = {
-		{ BTN_LEFT,  LV_KEY_PREV },
-		{ BTN_RIGHT, LV_KEY_NEXT },
-		{ BTN_ENTER, LV_KEY_ENTER },
-		{ BTN_BACK,  LV_KEY_ESC },
-		{ BTN_R,     LV_KEY_DEL }
-};
+std::map<uint8_t, lv_key_t> InputChatter::keyMap = {{BTN_UP,   LV_KEY_LEFT},
+													{BTN_DOWN, LV_KEY_RIGHT},
+													{BTN_A,    LV_KEY_ENTER},
+													{BTN_B,    LV_KEY_ESC},
+													{BTN_C,    LV_KEY_DEL}};
 
 
-InputChatter::InputChatter() : InputLVGL(LV_INDEV_TYPE_KEYPAD){
+InputChatter::InputChatter() : InputLVGL(LV_INDEV_TYPE_ENCODER){
 
 }
 
 void InputChatter::read(lv_indev_drv_t* drv, lv_indev_data_t* data){
 	if(lastKey == (uint32_t)-1) return;
 
-	auto key = KeyMap.find(lastKey);
-	if(key == KeyMap.end()) return;
-
-	data->key = key->second;
+	data->key = keyMap[lastKey];
 	data->state = pressed ? LV_INDEV_STATE_PRESSED : LV_INDEV_STATE_RELEASED;
 }
 
