98bc9beb35c1b32a1419c2c725f8aa5431bf4008
Filip Budiša <filip@circuitmess.com>
2022-02-08 17:32:52 +0100

    Add shutdown time to settings

--- src/Screens/SettingsScreen.h
+++ src/Screens/SettingsScreen.h
@@ -17,6 +17,7 @@ public:
 private:
 	lv_obj_t* sound;
 	lv_obj_t* sleepTime;
+	lv_obj_t* shutdownTime;
 	lv_obj_t* screenBrightness;
 	lv_obj_t* factoryReset;
 	lv_obj_t* HWTest;
@@ -24,7 +25,9 @@ private:
 
 	lv_obj_t* soundSwitch;
 	lv_obj_t* sleepTimeLabel;
+	lv_obj_t* shutdownTimeLabel;
 	lv_obj_t* sleepSlider;
+	lv_obj_t* shutdownSlider;
 	lv_obj_t* brightnessSlider;
 
 	lv_style_t style_knob;
