aa19d1ab883c315b0010f6b7e598b199d54666c0
Filip Budiša <filip@circuitmess.com>
2022-02-08 18:02:58 +0100

    Add fade in and fade out on first time test

--- Chatter-Firmware.ino
+++ Chatter-Firmware.ino
@@ -154,10 +154,13 @@ void setup(){
 		auto test = new UserHWTest([](){
 			Settings.get().tested = true;
 			Settings.store();
+			Chatter.fadeOut();
 			boot();
 		});
 
 		test->start();
+		lv_timer_handler();
+		Chatter.fadeIn();
 		return;
 	}
 
